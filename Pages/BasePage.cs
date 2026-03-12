1275bb9f26322467faad6289679df57b2580d374
Pablo <ortegaperaltap@gmail.com>
2026-03-11 22:41:46 -0400

    Create Pages Project for POM and add a BasePage class

---
+++ Pages/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Playwright;
+
+namespace Pages;
+
+public abstract class BasePage
+{
+  protected readonly IPage Page;
+
+  protected BasePage(IPage page)
+  {
+    Page = page;
+  }
+}
