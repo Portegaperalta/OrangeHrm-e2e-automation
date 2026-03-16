f7f087732e327b5a833c58c6804ce5a75fcf4a26
Pablo <ortegaperaltap@gmail.com>
2026-03-16 07:24:58 -0400

    Add base url

--- Tests/AuthTests.cs
+++ Tests/AuthTests.cs
@@ -1,7 +1,16 @@
-﻿using Pages;
+﻿using Microsoft.Playwright.Xunit;
+using Microsoft.Playwright;
+using Pages;
 
 namespace Tests;
 
-public class AuthTests
+public class AuthTests : PageTest
 {
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new BrowserNewContextOptions
+        {
+            BaseURL = "https://opensource-demo.orangehrmlive.com",
+        };
+    }
 }
