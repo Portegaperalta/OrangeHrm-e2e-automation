33983629340a569cfbc1fdf833b6246fd269c0d4
Pablo <ortegaperaltap@gmail.com>
2026-03-18 19:04:04 -0400

    Change candidates title locator

--- Pages/obj/Debug/net10.0/Pages.AssemblyInfo.cs
+++ Pages/obj/Debug/net10.0/Pages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1c1923e99e5484fbceb5a695a344a6fcd9c986")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282755ae6b9abfa2b719516f8dc101a7b85930e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
