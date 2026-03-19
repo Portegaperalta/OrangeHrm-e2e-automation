7e8ccf88a33fc18f615eb136a69dbffbb2c5dbaa
Pablo <ortegaperaltap@gmail.com>
2026-03-19 06:55:09 -0400

    Create POM class for PIM / Add Employees page

--- Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1a100cce1f8f78caebd666c9ebc2b9252d91c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1865d476014f9c832a303b5449534555bc5daab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
